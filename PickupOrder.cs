d80b25fbfc2a9f33df45d46b4768bb0ecd47487f
Rinxan <bikbulatovravil@mail.ru>
2025-12-17 00:30:28 +0300

    Pickup class added

---
+++ PickupOrder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem
+{
+    public class PickupOrder(int id) : Order(id)
+    {
+        public override void Process()
+        {
+            decimal productsTotal = CalculateTotal();
+
+            Console.WriteLine($"Order #{Id}");
+            Console.WriteLine($"Products total: {productsTotal}");
+            Console.WriteLine($"Pickup: (no delivery)");
+            Console.WriteLine($"Final total: {productsTotal}");
+        }
+    }
+}
