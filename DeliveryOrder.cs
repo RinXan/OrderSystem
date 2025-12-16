773346c61594b0a7ce450595f68fc69dfde516e0
Rinxan <bikbulatovravil@mail.ru>
2025-12-16 22:38:52 +0300

    DeliveryOrder class added

---
+++ DeliveryOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem
+{
+    public class DeliveryOrder : Order
+    {
+        public decimal DeliveryPrice { get; }
+        public DeliveryOrder(int id, decimal deliveryPrice) : base(id)
+        {
+            if (deliveryPrice <= 0) throw new ArgumentOutOfRangeException(nameof(deliveryPrice), "Delivery price should be greater than 0");
+            DeliveryPrice = deliveryPrice;
+        }
+        public override void Process()
+        {
+            decimal productsTotal = CalculateTotal();
+            decimal finalPrice = productsTotal + DeliveryPrice;
+
+            Console.WriteLine($"Order #{Id}");
+            Console.WriteLine($"Products total: {productsTotal}");
+            Console.WriteLine($"Delivery price: {DeliveryPrice}");
+            Console.WriteLine($"Final total: {finalPrice}");
+        }
+    }
+}
