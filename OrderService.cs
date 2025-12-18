45432677da490220e7cdb3c1f024325e8abf0a75
Rinxan <bikbulatovravil@mail.ru>
2025-12-19 02:16:57 +0300

    OrderService added

---
+++ OrderService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem
+{
+    public class OrderService
+    {
+        private readonly IProductRepository _repository;
+
+        public OrderService(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public void AddProductToOrder(Order order, string name)
+        {
+            if (order is null) throw new ArgumentException("Product name is empty", nameof(name)); ;
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+
+            Product product = _repository.GetByName(name);
+
+            if (product is null) throw new InvalidOperationException("Product not found");
+
+            order.AddProduct(product);
+        }
+    }
+}
