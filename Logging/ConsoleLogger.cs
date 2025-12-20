5e88237d194dea7b426183e7faeada69b09227b7
Rinxan <bikbulatovravil@mail.ru>
2025-12-20 17:44:13 +0300

    Adding Logger

---
+++ Logging/ConsoleLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[ERROR] {message}");
+            Console.ResetColor();
+        }
+
+        public void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[INFO] {message}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Logging/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Logging
+{
+    public interface ILogger
+    {
+        void Log(string  message);
+        void Error(string message);
+    }
+}
